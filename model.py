4952a7518f158034d700bcd62418d6b591d4c220
Vishnu-3101 <gadigevishnu.vardhan2021@vitstudent.ac.in>
2024-10-21 12:13:45 +0530

    Added codes for AFE block

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from torch import nn
 import torchvision
 from typing import Any, Callable, List, Optional, Type, Union
 from torch import Tensor
+from  torchvision.ops.deform_conv import DeformConv2d
 
 
 class MSFE(nn.Module):
@@ -23,7 +24,7 @@ class MSFE(nn.Module):
             norm_layer = nn.BatchNorm2d
         self.conv1 = nn.Conv2d(inplanes,outplanes,kernel_size=kernel_size,stride=stride)
         self.bn1 = norm_layer(outplanes)
-        self.dil_conv2 = nn.Conv2d(outplanes,outplanes,kernel_size=kernel_size,dilation=dilation)
+        self.dil_conv2 = nn.Conv2d(outplanes,outplanes,kernel_size=1,dilation=dilation)
         self.bn2 = norm_layer(outplanes)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -42,18 +43,49 @@ class MSFE(nn.Module):
         out = self.relu(out)
         return out
     
-inplanes = 3
-outplanes = 6
+
+class AFE(nn.Module):
+    def __init__(
+            self,
+            inplanes:int,
+            outplanes:int,
+            kernel_size:int=1,
+            stride:int=1,
+            downsample: Optional[nn.Module]=None,
+    )->None:
+        super().__init__()
+        self.depthwise1 = nn.Conv2d(in_channels=inplanes,out_channels = inplanes, kernel_size = 1,stride=1)
+        self.pointwise1 = nn.Conv2d(in_channels=inplanes,out_channels = inplanes, kernel_size = 1)
+        self.deform_conv = DeformConv2d(in_channels = inplanes, out_channels =inplanes, kernel_size=1,stride = 1)
+        self.depthwise2 = nn.Conv2d(in_channels=inplanes,out_channels = inplanes, kernel_size = kernel_size,stride=stride)
+        self.pointwise2 = nn.Conv2d(in_channels=inplanes,out_channels = outplanes, kernel_size = 1)
+        self.downsample = downsample
+    
+    def forward(self,x):
+        # identity = x
+        offset = x
+        print(offset.shape)
+        out = self.depthwise1(x)
+        out = self.pointwise1(out)
+        print(out.shape)
+        out = self.deform_conv(out,offset)
+        out = self.depthwise2(out)
+        out = self.pointwise2(out)
+        return out
+
+
+inplanes = 6
+outplanes = 12
 expansion = 1
 kernel_size = 2
 stride = 2
-dilation = 2
+dilation = 3
 downsample = nn.Sequential(
-                nn.Conv2d(inplanes, outplanes*expansion, kernel_size=5, stride=stride),
+                nn.Conv2d(inplanes, outplanes*expansion, kernel_size=kernel_size, stride=stride),
                 nn.BatchNorm2d(outplanes * expansion),
             )
-conv = MSFE(inplanes,outplanes,kernel_size,stride,downsample,dilation)
-input = torch.randn(20, 3, 224, 224)
+conv = AFE(inplanes,outplanes,kernel_size,stride)
+input = torch.randn(32, 6, 112, 112)
 output = conv(input)
 
 print(output.shape)
